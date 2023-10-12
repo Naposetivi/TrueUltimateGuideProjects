8a09d7dca06f4934ae73f5f29e908662337b35bc
Naposetivi <110626741+Naposetivi@users.noreply.github.com>
2023-10-13 00:11:33 +0300

    Here we have reviewed good practices for using unit tests, how to use mock DbContext, work with AutoFixture, why and how to create repositories and mock repositories. We also reviewed such things as Controller Unit Tests and Integration Test

--- CRUDExample/CRUDExample/Program.cs
+++ CRUDExample/CRUDExample/Program.cs
@@ -2,18 +2,26 @@ using ServiceContracts;
 using Services;
 using Microsoft.EntityFrameworkCore;
 using Entities;
+using RepositoryContracts;
+using Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 //add services into IoC container
+
+builder.Services.AddScoped<ICountriesRepository, CountriesRepository>();
+builder.Services.AddScoped<IPersonsRepository, PersonsRepository>();
+
 builder.Services.AddScoped<ICountriesService, CountriesService>();
 builder.Services.AddScoped<IPersonsService, PersonsService>();
 
-builder.Services.AddDbContext<PersonsDbContext>(options =>
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+//Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=PersonsDatabase;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False
 
 var app = builder.Build();
 
@@ -22,10 +30,13 @@ if (builder.Environment.IsDevelopment())
     app.UseDeveloperExceptionPage();
 }
 
-Rotativa.AspNetCore.RotativaConfiguration.Setup("wwwroot", wkhtmltopdfRelativePath: "Rotativa");
+if (builder.Environment.IsEnvironment("Test") == false)
+    Rotativa.AspNetCore.RotativaConfiguration.Setup("wwwroot", wkhtmltopdfRelativePath: "Rotativa");
 
 app.UseStaticFiles();
 app.UseRouting();
 app.MapControllers();
 
 app.Run();
+
+public partial class Program { } //make the auto-generated Program accessible programmatically
--- CRUDExample/Entities/Migrations/20231009060311_TaxIdentificationNumber.Designer.cs
+++ CRUDExample/Entities/Migrations/20231009060311_TaxIdentificationNumber.Designer.cs
@@ -11,7 +11,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Entities.Migrations
 {
-    [DbContext(typeof(PersonsDbContext))]
+    [DbContext(typeof(ApplicationDbContext))]
     [Migration("20231009090311_TaxIdentificationNumber")]
     partial class TaxIdentificationNumber
     {
